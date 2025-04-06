c2364e8f02241aec53f6488e7e685eddcf1a5e12
marcelnoehre <marcelnoehre@web.de>
2025-04-06 13:16:51 +0200

    add controllers

--- controllers/interpreter-controller.js
+++ controllers/interpreter-controller.js
@@ -1,27 +1,27 @@
-const lexer = require('../services/lexer-service');
+const interpreter = require('../services/interpreter-service');
 
 function ping(req, res) {
   try {
-    const result = lexer.ping();
+    const result = interpreter.ping();
     res.json({ result });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
 
-function tokenize(req, res) {
+function interpret(req, res) {
   try {
     const input = req.body;
-    const result = lexer.tokenize(input);
-    res.json({ tokens: result });
+    const result = interpreter.interpret(input);
+    res.json({ result: result });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
 
 function registerRoutes(app) {
-  app.get('/api/lexer', ping);
-  app.post('/api/lexer/tokenize', tokenize);
+  app.get('/api/interpreter', ping);
+  app.post('/api/interpreter/interpret', interpret);
 }
 
 module.exports = {
--- controllers/parser-controller.js
+++ controllers/parser-controller.js
@@ -1,27 +1,27 @@
-const lexer = require('../services/lexer-service');
+const parser = require('../services/parser-service');
 
 function ping(req, res) {
   try {
-    const result = lexer.ping();
+    const result = parser.ping();
     res.json({ result });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
 
-function tokenize(req, res) {
+function parse(req, res) {
   try {
     const input = req.body;
-    const result = lexer.tokenize(input);
-    res.json({ tokens: result });
+    const result = parser.parse(input);
+    res.json({ ast: result });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 }
 
 function registerRoutes(app) {
-  app.get('/api/lexer', ping);
-  app.post('/api/lexer/tokenize', tokenize);
+  app.get('/api/parser', ping);
+  app.post('/api/parser/parse', parse);
 }
 
 module.exports = {
