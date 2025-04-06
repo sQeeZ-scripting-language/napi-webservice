bcb4ed33d7c1925e8482195a736d38c8a10fb18e
marcelnoehre <marcelnoehre@web.de>
2025-04-06 13:19:49 +0200

    register parser and interpreter routes in server setup

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const lexer = require('./controllers/lexer-controller');
+const parser = require('./controllers/parser-controller');
+const interpreter = require('./controllers/interpreter-controller');
 
 app.use(express.json());
 
 lexer.registerRoutes(app);
+parser.registerRoutes(app);
+interpreter.registerRoutes(app);
 
 app.listen(port, () => {
   console.log(`API running on port ${port}`);
